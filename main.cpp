90255c648262062374073517bddab3857a8951c2
Nehemias Herrera <yo@nehemiascr.com>
2015-05-03 19:56:02 -0600

    cambio en los nombres de variables
    para que se entienda mejor

--- main.cpp
+++ main.cpp
@@ -14,13 +14,13 @@ using namespace std;
 /*
  * Función que devuleve un arreglo con los nombres de los archivos en el directorio path
  */
-void getDirectorio(string path, vector<string> &archivos){
+void getDirectorio(string path, vector<string> &directorio){
     DIR *d = opendir(path.c_str());
     struct dirent *dir;
     
     if (d){
         while ((dir = readdir(d)) != NULL){
-            archivos.push_back(path + dir->d_name);
+            directorio.push_back(path + dir->d_name);
         }
         closedir(d);
     }
@@ -39,7 +39,7 @@ void imprimir(vector<string> &v){
 /*
  * Función que pregunta al usuario los hilos a ejecutar
  */
-void preguntarDatos(vector<string> &hilos){
+void preguntarDatos(vector<string> &nombres_de_archivo){
     
     // Preguntar la cantidad de hilos a ejecutar
     int cantidad_de_hilos;
@@ -48,38 +48,34 @@ void preguntarDatos(vector<string> &hilos){
     
     // Preguntar el directorio donde se encuentran los hilos
     string path;
-    cout << "Donde se encuentran los hilos ? ->  ";
+    cout << "Donde se encuentran los hilos ?, digite ./ para el directorio actual ->  ";
     cin >> path;
     
-    vector<string> archivos;
-    getDirectorio(path, archivos);
+    // Obtener todos los archivos del directorio path
+    vector<string> directorio;
+    getDirectorio(path, directorio);
+    imprimir(directorio);
     
-    imprimir(archivos);
-    
-    vector<int> indices_de_archivo;
-    
+    // Preguntar por los hijos a ejecutar
     for (int i = 1; i <= cantidad_de_hilos; i++) {
         cout << "Digite el indice del " << i << "º hilo ->  ";
         int temporal;
         cin >> temporal;
-        indices_de_archivo.push_back(temporal);
+        nombres_de_archivo.push_back(directorio[temporal]); // Almacenar el nombre del hilo a ejecutar
     }
-    
-    for (int i = 0; i < indices_de_archivo.size(); i++) {
-        hilos.push_back(archivos[indices_de_archivo[i]]);
-    }
-    
 }
 
 /*
- * Función que lee los hilos y los almacena en un vector
+ * Función que lee nombres_de_hilos y almacena las intrucciones un arreglo
  */
-void getInstrucciones(vector<string> &nombres_de_archivo, vector<int> &instrucciones){
-    
-    for (int i = 0; i < nombres_de_archivo.size(); i++) {
-        ifstream infile(nombres_de_archivo[i].c_str());
-        int opcode,rs,rt,rd;
-        while (infile >> opcode >> rs >> rt >> rd) {
+void getInstrucciones(vector<string> &nombres_de_archivo, vector<int> &instrucciones, vector<int> &indices_de_hilos){
+
+    for (int i = 0; i < nombres_de_archivo.size(); i++) { //por cada nombre de hilo
+        indices_de_hilos.push_back(instrucciones.size()); // almacenar el indice donde empieza
+        ifstream infile(nombres_de_archivo[i].c_str()); // abrir archivo
+        int opcode,rs,rt,rd; // partes de la instruccion
+        while (infile >> opcode >> rs >> rt >> rd) { //cargar instruccion
+            // almacenar la instruccion en e; arreglo de instrucciones
             instrucciones.push_back(opcode);
             instrucciones.push_back(rs);
             instrucciones.push_back(rt);
@@ -88,22 +84,34 @@ void getInstrucciones(vector<string> &nombres_de_archivo, vector<int> &instrucci
     }
 }
 
-
 int main(){
+
+    vector<string> nombres_de_archivo_de_hilos; // nombres de archivo donde se encuentran los hilos
+    vector<int>    instrucciones; // arreglo con todas las instrucciones de cada hilo
+    vector<int>    indices_de_hilos; // indices de donde comienza cada hilo en el arreglo de instrucciones
     
-    vector<string> hilos;
-    vector<int>    instrucciones;
-    
-    preguntarDatos(hilos);
-    getInstrucciones(hilos, instrucciones);
-    
-    for (int i = 0; i < instrucciones.size(); i++) {
-        cout << "[" << instrucciones[i] << "] ";
-        if ((i+1)%4 == 0) cout << endl;
-        
+    preguntarDatos(nombres_de_archivo_de_hilos); // Obtener datos del usuario
+    getInstrucciones(nombres_de_archivo_de_hilos, instrucciones, indices_de_hilos); //cargar los hilos en el arreglo de instrucciones
+
+    cout << indices_de_hilos.size() << " hilos seleccionados" << endl;
+    for (int i = 0; i < indices_de_hilos.size(); i++) {
+        cout << i << " -> " << indices_de_hilos[i] << endl;;
     }
     
-    
+
+    for (int hilo = 0; hilo < indices_de_hilos.size(); hilo++) {
+        cout << "Ejecutando hilo " << hilo << ", " << nombres_de_archivo_de_hilos[hilo] << endl;
+        int primero = indices_de_hilos[hilo];
+        int ultimo;
+        if (hilo == indices_de_hilos.size() - 1) ultimo = instrucciones.size() - 1;
+        else ultimo = indices_de_hilos[hilo + 1];
+        
+        for (int h = primero; h <= ultimo; h++) {
+            cout << "[" << instrucciones[h] << "] ";
+            if ((h+1)%4 == 0) cout << endl;
+
+        }
+    }
     
     return 0;
 }
